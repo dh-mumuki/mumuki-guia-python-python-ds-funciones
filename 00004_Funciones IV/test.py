27d140e4bfec60e5bd0e440a58cd8a947f260fd2
andres@digitalhouse.com <andres@digitalhouse.com>
2018-12-01 15:05:22 -0500

    Mumuki Export on 2018-12-01 15:05:22 -0500

--- 00004_Funciones IV/test.py
+++ 00004_Funciones IV/test.py
@@ -1,1 +1,6 @@
-#
+class TestFixtures(unittest.TestCase):
+  def test_listas(self):
+    self.assertEquals(contaor([2, 5, 4, 8, 9, 3, 5 , 6]),  8.0)
+    
+  def test_listas(self):
+    self.assertEquals(media([2, 5, 4, 8, 9, 3, 5 , 6]),  5.25)    
