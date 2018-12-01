7dff74cf068c9608e8eed795d8ef2c04b178a8db
andres@digitalhouse.com <andres@digitalhouse.com>
2018-12-01 15:25:53 -0500

    Mumuki Export on 2018-12-01 15:25:53 -0500

--- 00005_Funciones V/test.py
+++ 00005_Funciones V/test.py
@@ -1,3 +1,3 @@
 class TestFixtures(unittest.TestCase):
   def test_listas(self):
-    self.assertEquals(lista_par_impar(), ([2, 4, 8, 6, 0], [5, 9, 3, 5]))
+    self.assertEquals(lista_par_impar([2, 5, 4, 8, 9, 3, 5 , 6, 0]), ([2, 4, 8, 6, 0], [5, 9, 3, 5]))
