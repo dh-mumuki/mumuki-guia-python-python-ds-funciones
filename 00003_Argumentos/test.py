5932d0b8e295611b3393148df609f1938f18ab5e
andres@digitalhouse.com <andres@digitalhouse.com>
2018-12-14 16:29:54 -0500

    Mumuki Export on 2018-12-14 16:29:54 -0500

--- 00003_Argumentos/test.py
+++ 00003_Argumentos/test.py
@@ -1,12 +1,12 @@
 class TestFixtures(unittest.TestCase):
-  def test_listas(self):
+  def test_listas_1(self):
     self.assertEquals(tipo_de_dato(5), False)
     
-  def test_listas(self):
+  def test_listas_2(self):
     self.assertEquals(tipo_de_dato(5.0), True)
     
-  def test_listas(self):
+  def test_listas_3(self):
     self.assertEquals(tipo_de_dato("cinco"), True)
     
-  def test_listas(self):
+  def test_listas_4(self):
     self.assertEquals(tipo_de_dato(True), False)
