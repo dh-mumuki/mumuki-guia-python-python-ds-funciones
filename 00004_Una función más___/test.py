d41f4c48b64e2cce26218c1fb9779976a57d1260
andres@digitalhouse.com <andres@digitalhouse.com>
2018-12-14 16:39:07 -0500

    Mumuki Export on 2018-12-14 16:39:07 -0500

--- 00004_Una función más___/test.py
+++ 00004_Una función más___/test.py
@@ -1,6 +1,6 @@
 class TestFixtures(unittest.TestCase):
-  def test_listas(self):
-    self.assertEquals(contaor([2, 5, 4, 8, 9, 3, 5 , 6]),  8.0)
+  def test_listas_len(self):
+    self.assertEquals(contador([2, 5, 4, 8, 9, 3, 5 , 6]),  8)
     
-  def test_listas(self):
+  def test_listas_media(self):
     self.assertEquals(media([2, 5, 4, 8, 9, 3, 5 , 6]),  5.25)    
