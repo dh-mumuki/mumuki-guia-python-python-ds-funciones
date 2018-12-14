1e99600ebb3a609792df3ff8d3de9091460befea
andres@digitalhouse.com <andres@digitalhouse.com>
2018-12-14 16:21:17 -0500

    Mumuki Export on 2018-12-14 16:21:17 -0500

--- 00002_Usando funciones/test.py
+++ 00002_Usando funciones/test.py
@@ -4,7 +4,7 @@ class TestFixtures(unittest.TestCase):
     self.assertEquals(mitad(4),2)
   
   def test_mitad_negativo(self):
-    self.asserEquals(mitad(-4), -2)
+    self.assertEquals(mitad(-4), -2)
   
   def test_mitad_negativo_float(self):
     self.assertAlmostEqual(mitad(-4.5), -2.25)
