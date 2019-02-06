b979a6c49fcbe51150347daddfbad62e9ccf848e
Andres Furlan <Andres Furlan>
2019-02-06 19:08:19 +0000

    Mumuki Export on 2019-02-06 19:08:19 +0000

--- 00009_Funcion/test.py
+++ 00009_Funcion/test.py
@@ -1,7 +1,7 @@
 class TestFixtures(unittest.TestCase):
-  def Test_2(self):
+  def Test2(self):
     self.assertEquals(cuadrado_de_suma(1, 2), 9)
     
-  def Test_1(self):
+  def Test2(self):
     self.assertEquals(cuadrado_de_suma(-10, 3), 49)
   
