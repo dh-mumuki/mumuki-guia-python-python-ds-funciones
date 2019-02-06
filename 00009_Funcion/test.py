ab95e772518a9ef7c7b6ec2168eb4b8e73e800c2
Andres Furlan <Andres Furlan>
2019-02-06 18:43:31 +0000

    Mumuki Export on 2019-02-06 18:43:31 +0000

--- 00009_Funcion/test.py
+++ 00009_Funcion/test.py
@@ -3,5 +3,5 @@ class TestFixtures(unittest.TestCase):
     self.assertEquals(cuadrado_de_suma(1, 2), 9, "Hay algo en la función que no está bien")
     
   def test_sum_q_2(self):
-    self.assertEquals(cuadrado_de_suma(-10, 3),49)
+    self.assertEquals(cuadrado_de_suma(-10, 3),49,  "Hay algo en la función que no está bien")
   
