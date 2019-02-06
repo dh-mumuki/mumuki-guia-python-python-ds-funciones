72adec316072f914c8c6dafe8607d57eb416eb10
Andres Furlan <Andres Furlan>
2019-02-06 19:02:12 +0000

    Mumuki Export on 2019-02-06 19:02:12 +0000

--- 00009_Funcion/test.py
+++ 00009_Funcion/test.py
@@ -1,7 +1,7 @@
 class TestFixtures(unittest.TestCase):
   def Mismo_signo_al_cuadrado(self):
-    self.assertEquals(cuadrado_de_suma(1, 2), 9, "Revisa la funcion porque algo no esta bien")
+    self.assertEquals(cuadrado_de_suma(1, 2), 9)
     
   def Distinto_signo_al_cuadrado(self):
-    self.assertEquals(cuadrado_de_suma(-10, 3),49, "Revisa la funcion porque algo no esta bien")
+    self.assertEquals(cuadrado_de_suma(-10, 3), 49)
   
