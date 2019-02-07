80b0ac0c9f4977ba179f77cfcbdb4c0ad3499e3a
Andres Furlan <Andres Furlan>
2019-02-07 15:30:28 +0000

    Mumuki Export on 2019-02-07 15:30:28 +0000

--- 00003_Argumentos/test.py
+++ 00003_Argumentos/test.py
@@ -1,12 +1,12 @@
 class TestFixtures(unittest.TestCase):
   def test_listas_1(self):
-    self.assertEquals(tipo_de_dato(5), False)
+    self.assertEquals(tipo_de_dato(5), False, 'Para un entero positivo la funcion debe devolver False')
     
   def test_listas_2(self):
-    self.assertEquals(tipo_de_dato(5.0), True)
+    self.assertEquals(tipo_de_dato(5.0), True, 'Para un float la funcion debe devolver True')
     
   def test_listas_3(self):
-    self.assertEquals(tipo_de_dato("cinco"), True)
+    self.assertEquals(tipo_de_dato("cinco"), True, 'Para un string la funcion debe devolver True')
     
   def test_listas_4(self):
-    self.assertEquals(tipo_de_dato(True), False)
+    self.assertEquals(tipo_de_dato(True), False, 'Para un booleano la funcion debe devolver False')
