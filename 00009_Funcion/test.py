74c6495cba69f0676f7ee442f75950bb7954bf34
Andres Furlan <Andres Furlan>
2019-02-07 15:35:32 +0000

    Mumuki Export on 2019-02-07 15:35:32 +0000

--- 00009_Funcion/test.py
+++ 00009_Funcion/test.py
@@ -1,7 +1,7 @@
 class TestFixtures(unittest.TestCase):
   def test_mismo_signo(self):
-    self.assertEquals(cuadrado_de_suma(1, 2), 9, "Revisa la funcion")
+    self.assertEquals(cuadrado_de_suma(1, 2), 9, "Revisa la funcion, debes sumar primero y a ese resultado aplicar el cuadrado")
     
   def test_distinto_signo(self):
-    self.assertEquals(cuadrado_de_suma(-10, 3), 49, "Revisa la funcion")
+    self.assertEquals(cuadrado_de_suma(-10, 3), 49, "Revisa la funcion, cuando el signo es negativo, la suma se transforma en resta.")
   
