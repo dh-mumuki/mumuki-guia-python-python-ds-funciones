01bb5d067535fbd9c8626103195d9e749d43eafa
Andres Furlan <Andres Furlan>
2019-02-07 15:26:27 +0000

    Mumuki Export on 2019-02-07 15:26:27 +0000

--- 00002_Usando funciones/test.py
+++ 00002_Usando funciones/test.py
@@ -1,10 +1,10 @@
 class TestFixtures(unittest.TestCase):
   
   def test_mitad_entero(self):
-    self.assertEquals(mitad(4),2)
+    self.assertEquals(mitad(4),2, 'La funcion no entrega el resultado correcto cuando el valor es un entero positivo')
   
   def test_mitad_negativo(self):
-    self.assertEquals(mitad(-4), -2)
+    self.assertEquals(mitad(-4), -2, 'La funcion no devuelve el resultado correcto cuando el numero es negativo')
   
   def test_mitad_negativo_float(self):
-    self.assertAlmostEqual(mitad(-4.5), -2.25)
+    self.assertAlmostEqual(mitad(-4.5), -2.25, 'La funcion no devuelve el valor esperado cuando el numero es de tipo float negativo')
