80e471b13093cd54d8cdf4235d87e94088c2c1df
Andres Furlan <Andres Furlan>
2019-02-07 15:28:39 +0000

    Mumuki Export on 2019-02-07 15:28:39 +0000

--- 00007_Practiquemos un poquito más___/test.py
+++ 00007_Practiquemos un poquito más___/test.py
@@ -1,6 +1,6 @@
 class TestFixtures(unittest.TestCase):
   def test_dos(self):
-    self.assertEquals(producto(2),4)
+    self.assertEquals(producto(2, 2),4, 'El producto entre dos numeros positivos es incorrecto')
   
   def test_negativo(self):
-    self.assertEquals(producto(-4),16)
+    self.assertEquals(producto(-4, 4),-16, 'El producto entre un numero negativo y otro positivo es incorrecto')
