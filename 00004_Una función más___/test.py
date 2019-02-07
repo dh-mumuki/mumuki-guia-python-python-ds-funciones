6e8157924f3e933831c7f5cd3c030a0a2c8bec8c
Andres Furlan <Andres Furlan>
2019-02-07 15:33:40 +0000

    Mumuki Export on 2019-02-07 15:33:40 +0000

--- 00004_Una función más___/test.py
+++ 00004_Una función más___/test.py
@@ -1,6 +1,6 @@
 class TestFixtures(unittest.TestCase):
-  def test_listas_len(self):
-    self.assertEquals(contador([2, 5, 4, 8, 9, 3, 5 , 6]),  8)
+  def test_listas_cantidad_elementos(self):
+    self.assertEquals(contador([2, 5, 4, 8, 9, 3, 5 , 6]),  8, 'La funcion no devuelve la cantidad de elementos requerida')
     
   def test_listas_media(self):
-    self.assertEquals(media([2, 5, 4, 8, 9, 3, 5 , 6]),  5.25)    
+    self.assertEquals(media([2, 5, 4, 8, 9, 3, 5 , 6]),  5.25, 'La funcion no computa adecuadamente la media.')    
