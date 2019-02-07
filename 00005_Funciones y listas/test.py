2f2ef28921501e7449198b6eae2e4291454f8088
Andres Furlan <Andres Furlan>
2019-02-07 15:32:20 +0000

    Mumuki Export on 2019-02-07 15:32:20 +0000

--- 00005_Funciones y listas/test.py
+++ 00005_Funciones y listas/test.py
@@ -1,3 +1,3 @@
 class TestFixtures(unittest.TestCase):
   def test_listas(self):
-    self.assertEquals(lista_par_impar([2, 5, 4, 8, 9, 3, 5 , 6, 0]), ([2, 4, 8, 6, 0], [5, 9, 3, 5]))
+    self.assertEquals(lista_par_impar([2, 5, 4, 8, 9, 3, 5 , 6, 0]), ([2, 4, 8, 6, 0], [5, 9, 3, 5]), 'La funcion clasifica de forma incorrecta, la primera lista debe contener los pares y la segunda los impares.')
