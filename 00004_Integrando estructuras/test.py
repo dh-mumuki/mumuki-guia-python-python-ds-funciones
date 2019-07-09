dd785e01b9e72872d2a6cd34ca9e09ba23a75a05
andres@digitalhouse.com <andres@digitalhouse.com>
2019-07-09 15:58:54 +0000

    Mumuki Export on 2019-07-09 15:58:54 +0000

--- 00004_Integrando estructuras/test.py
+++ 00004_Integrando estructuras/test.py
@@ -3,4 +3,4 @@ class TestFixtures(unittest.TestCase):
     self.assertEquals(contador([2, 5, 4, 8, 9, 3, 5 , 6]),  8, 'La funcion no devuelve la cantidad de elementos requerida')
     
   def test_listas_media(self):
-    self.assertEquals(media([2, 5, 4, 8, 9, 3, 5, 6, 10, 15]),  6.7, 'La funcion no computa adecuadamente la media.')
+    self.assertEquals(media([2, 5, 4, 8, 9, 3, 5, 6, 10, 15]),  6, 'La funcion no computa adecuadamente la media.')
